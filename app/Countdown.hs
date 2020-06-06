690f4e1d47281924bba74085482f350d324905de
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-06-06 11:32:44 +0100

    built function solutions that returns all possible expressions on top of a given list of numbers, valid to target another number

---
+++ app/Countdown.hs
@@ -0,0 +1,11 @@
+module Countdown where
+
+import Solution
+import Expr
+import Combinations
+
+main :: IO ()
+main = print (solutions [1,3,7,10,25,50] 765)
+
+-- $ stack build ------- check out the specific ghc switch -main-is added in .cabal file
+-- $ stack exec haskell-countdown-exe
--- app/Main.hs
+++ app/Main.hs
@@ -13,6 +13,6 @@ main = do
         putStrLn $ show $ interleave 1 [2,3,4]
         putStrLn $ show $ perms [1,2,3]
         putStrLn $ show $ choices [1,2,3]
-        putStrLn $ show $ solution (App Mul (App Add (Val 1) (Val 50)) (App Sub (Val 25) (Val 10))) [1,3,7,10,25,50] 765
-        putStrLn $ show $ solutions [1,3,7,10,25,50] 765
 
+-- $ stack build
+-- $ stack exec haskell-main-exe
